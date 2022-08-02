4e40ea9c69e587140ec51c7d6022fa66418825cc
Deem <Deem@DEEM>
2022-08-02 13:42:41 +0300

    Реализован ввод исходных данных

--- AngleWatch/Program.cs
+++ AngleWatch/Program.cs
@@ -8,8 +8,62 @@ namespace AngleWatch
 {
     internal class Program
     {
+
+        static TimeWatch timeWatch;
         static void Main(string[] args)
         {
+            Greet();
+            CalculateAngle();
+            Console.ReadLine();
+        }
+
+
+        private static void CalculateAngle()
+        {
+            Console.WriteLine("Введите время в формате 'hour:minute'");
+            timeWatch = new TimeWatch();
+            //входные данные
+
+            while (true)
+            {
+                string inputTime = Console.ReadLine();
+                string[] times = inputTime.Split(':');
+
+                timeWatch.hour = Convert.ToInt32(times[0]);
+                timeWatch.minute = Convert.ToInt32(times[1]);
+                if (timeWatch.hour > 24 || timeWatch.hour < 0)
+                {
+                    Console.WriteLine("Неверное время, укажите часы корректно от 0 до 24");
+                    continue;
+                }
+                if (timeWatch.minute > 60 || timeWatch.minute < 0)
+                {
+                    Console.WriteLine("Неверное время, укажите минуты корректно от 0 до 60");
+                    continue;
+                }
+                
+                break;
+
+            }
+
+            // расчет
+
+
+
+            
+            
+            
+            
+
+
+
+        }
+
+      
+
+        private static void Greet()
+        {
+            Console.WriteLine("Расчет градуса угла между часовой стрелкой и минутной");
         }
     }
 }
---
+++ AngleWatch/TimeWatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngleWatch
+{
+    internal class TimeWatch
+    {
+        public double hour { get; set; }
+        public double minute { get; set; }
+
+        public TimeWatch(double hour, double minute)
+        {
+            
+            this.hour = hour;
+            this.minute = minute;
+        }
+        public TimeWatch()
+        {
+
+        }
+    }
+}
